60a5da51b046ad70666989e3fe1b6b2146224a29
Matheus Fischer <matheusfischerr@icloud.com>
2020-06-11 11:11:03 -0300

    Adding appointments models and repositories

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
+import appointmentsRouter from './appointments.routes';
 
 const routes = Router();
+routes.use('/appointments', appointmentsRouter);
 
 routes.get('/', (request, response) => {
   const { name, email } = request.body;
