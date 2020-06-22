0e0a228fb8fb95cbf43c82934b74165bbf65434e
Matheus Fischer <matheusfischerr@icloud.com>
2020-06-22 19:18:13 -0300

    add cors to application

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import express, { Request, Response, NextFunction } from 'express';
 
 import 'express-async-errors';
 
+import cors from 'cors';
+
 import routes from './routes';
 
 import uploadConfig from './config/upload.config';
@@ -13,6 +15,9 @@ import AppError from './errors/AppError';
 import './database/index';
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 
 app.use('/files', express.static(uploadConfig.directory));
